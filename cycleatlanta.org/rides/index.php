96d3a0e4ef6db152dc79e48028239d3c68b8cab5
Corey Acri <acri.corey@gmail.com>
2013-07-10 19:46:32 -0400

    Deleted Google Analytics Script
    There was a google analytics script in head tag.  This was probably
    sending bad data to the folks in Atl, so I deleted it.

--- cycleatlanta.org/rides/index.php
+++ cycleatlanta.org/rides/index.php
@@ -16,17 +16,7 @@
         <link rel="stylesheet" href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.10/themes/base/jquery-ui.css" type="text/css" media="all" />
         <link rel="stylesheet" href="css/main.css">
 
-        <script type="text/javascript">	
-		  var _gaq = _gaq || [];
-		  _gaq.push(['_setAccount', 'UA-35489732-1']);
-		  _gaq.push(['_trackPageview']);
-		
-		  (function() {
-		    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-		    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-		    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-		  })();
-	</script>
+        
     </head>
     <body>
         <!--[if lt IE 7]>
