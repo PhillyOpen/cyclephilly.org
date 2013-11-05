12775692881c75805209a898090d16637bcbbcf9
Lloyd Emelle <lloyd@emelle.me>
2013-11-05 20:38:21 +0000

    Removed Device ID check

--- cycleatlanta.org/post/index.php
+++ cycleatlanta.org/post/index.php
@@ -78,7 +78,7 @@ elseif ( $version == PROTOCOL_VERSION_4 ) {
 }
 
 // validate device ID: should be 32 but some android devices are reporting 31
-if ( is_string( $device ) && strlen( $device ) === 32 || strlen( $device ) === 31)
+if ( is_string( $device ) /*&& strlen( $device ) === 32 || strlen( $device ) === 31*/)
 {
 	// try to lookup user by this device ID
 	$user = null;
